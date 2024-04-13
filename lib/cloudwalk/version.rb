a0c93bd8dde43ee0e65600098462684a7a8edc92
Willian Blanck <will-blk@hotmail.com>
2024-04-13 11:14:33 -0300

    Initial commit

---
+++ lib/cloudwalk/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Cloudwalk
+  VERSION = "0.1.0"
+end
---
+++ spec/cloudwalk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cloudwalk do
+  it "has a version number" do
+    expect(Cloudwalk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
