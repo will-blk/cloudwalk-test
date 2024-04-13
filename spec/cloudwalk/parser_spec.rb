0000bd9d57253c1d7105c6c3eba8fe880e2dd08c
Willian Blanck <will-blk@hotmail.com>
2024-04-13 17:00:21 -0300

    Add test with kills and deaths

--- spec/cloudwalk/parser_spec.rb
+++ spec/cloudwalk/parser_spec.rb
@@ -25,5 +25,30 @@ RSpec.describe Cloudwalk::Parser do
         expect(game.matches.size).to eq(1)
       end
     end
+
+
+    context "with multiple players and kills" do
+      subject(:game) { parser.parse }
+
+      let(:file) { "file_2.log" }
+
+      it "has the correct number of kills" do
+        expect(game.matches.first.total_kills).to eq(11)
+      end
+
+      it "has the correct players" do
+        expect(game.players.keys).to eq(["Isgalamido", "Dono da Bola", "Mocinha"])
+      end
+
+      it "has correct kills count" do
+        expect(game.matches.first.kills).to eq(
+          {
+            "Dono da Bola" => 0,
+            "Isgalamido" => 3,
+            "Mocinha" => 0
+          }
+        )
+      end
+    end
   end
 end
